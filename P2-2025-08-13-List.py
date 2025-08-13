ab03026b52ed432117d2993759a63455861874ec
AadiHarsha <jitendrawaghale@gmail.com>
2025-08-13 21:48:20 +0530

    List

---
+++ P2-2025-08-13-List.py
@@ -0,0 +1,6 @@
+# List Example
+fruits = ["Apple", "Banana", "Cherry"]
+fruits.append("Mango")   # Add
+fruits.remove("Banana")  # Remove
+print(f"Fruits List: {fruits}")
+print(f"First fruit: {fruits[0]}")
